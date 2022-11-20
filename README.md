41591800126af1ff634881a00446cd636ba9cb7b
jsamwell <jsamwellgb@gmail.com>
2022-11-20 20:39:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# learn-html-css
+ Estudos em HTML e CSS
---
+++ sites/site1/java.js
@@ -0,0 +1,8 @@
+VanillaTilt.init(document.querySelectorAll(".box"),
+    {
+        max: 5,
+        speed: 100,
+        easing: "cubix-bezier(.03,.98,.52,.99)",
+        perspective: 500,
+        transition: true
+    });
